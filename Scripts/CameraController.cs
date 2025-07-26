30b9f3cef14b568d179fb823b14cc242996649e4
denetor <denetor@gmail.com>
2025-07-26 21:48:04 +0200

    #17 camera follow player

---
+++ Scripts/CameraController.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Transform player;
+    private Vector3 offset = new Vector3(0f, 0f, -5f);
+    private float smoothTime = 0.5f;
+    private Vector3 velocity = Vector3.zero;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void LateUpdate()
+    {
+        Vector3 targetPosition = player.transform.position + offset;
+        transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTime);
+    }
+}
