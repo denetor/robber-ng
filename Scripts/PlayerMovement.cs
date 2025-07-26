bf1d97d89e4f472780aaa97dee2a13cb64ee24d4
denetor <denetor@gmail.com>
2025-07-26 21:22:25 +0200

    #2 player movement and animatins

--- Scripts/PlayerMovement.cs
+++ Scripts/PlayerMovement.cs
@@ -4,10 +4,12 @@ using UnityEngine.InputSystem;
 public class PlayerMovement : MonoBehaviour
 {
     public Rigidbody2D rb;
+    public Animator animator;
     public float horizontalSpeed = 5f;
     public float verticalSpeed = 5f;
     float horizontalMovement;
     float verticalMovement;
+    int horizontalHeading = 1;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -19,6 +21,17 @@ public class PlayerMovement : MonoBehaviour
     void Update()
     {
         rb.linearVelocity = new Vector2(horizontalMovement * horizontalSpeed, verticalMovement * verticalSpeed);
+
+        // flip sprite if heading changed
+        if (horizontalMovement > 0) {
+            horizontalHeading = 1;
+        } else if (horizontalMovement < 0) {
+            horizontalHeading = -1;
+        }
+        rb.GetComponent<SpriteRenderer>().flipX = (horizontalHeading < 0);
+
+        // set animation parameters to apply transitions, if needed
+        animator.SetFloat("magnitude", rb.linearVelocity.magnitude);
     }
 
     public void Move(InputAction.CallbackContext context)
