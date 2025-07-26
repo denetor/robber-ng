4c2d1f6bb4fcbd7db68b755232291c10b68c3a6b
denetor <denetor@gmail.com>
2025-07-27 00:47:37 +0200

    #3 add: collectable items WIP

---
+++ Scripts/Interfaces/ICollectable.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+/**
+  Interface that describes an item that a creature can pick up and put in his inventory
+*/
+public interface ICollectable
+{
+    public void Collect();
+}
