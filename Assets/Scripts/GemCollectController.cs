a6b4f2a3f077c4fb0d874f0b4075b7160dcaa29e
denetor <denetor@gmail.com>
2025-08-03 13:40:15 +0200

    #18 particle effect when collecting items

--- Assets/Scripts/GemCollectController.cs
+++ Assets/Scripts/GemCollectController.cs
@@ -5,6 +5,8 @@ using UnityEngine.Events;
 public class GemCollectController : MonoBehaviour, ICollectable
 {
     public static UnityEvent GemCollectEvent;
+    [SerializeField] private ParticleSystem collectEffectPrefab;
+    
 
     void Start() {
         if (GemCollectEvent == null) {
@@ -14,7 +16,13 @@ public class GemCollectController : MonoBehaviour, ICollectable
 
     public void Collect() {
         // publish event
+        Debug.Log("Gem collected");
         GemCollectEvent.Invoke();
+
+        // instantiate particle system
+        ParticleSystem collectParticle;
+        collectParticle = Instantiate(collectEffectPrefab, transform.position, Quaternion.identity);
+        collectParticle.Play();
 
         // destroy this item
         Destroy(gameObject);
--- Assets/Scripts/GoldKeyCollectController.cs
+++ Assets/Scripts/GoldKeyCollectController.cs
@@ -5,6 +5,7 @@ using UnityEngine.Events;
 public class GoldKeyCollectController : MonoBehaviour, ICollectable
 {
     public static UnityEvent GoldKeyCollectEvent;
+    [SerializeField] private ParticleSystem collectEffectPrefab;
 
     void Start() {
         if (GoldKeyCollectEvent == null) {
@@ -15,6 +16,11 @@ public class GoldKeyCollectController : MonoBehaviour, ICollectable
     public void Collect() {
         // publish event
         GoldKeyCollectEvent.Invoke();
+
+        // particle effect
+        ParticleSystem collectParticle;
+        collectParticle = Instantiate(collectEffectPrefab, transform.position, Quaternion.identity);
+        collectParticle.Play();
 
         // destroy this item
         Destroy(gameObject);
